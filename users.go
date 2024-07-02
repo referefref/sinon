e158173818b588b128b46a49f3bc737824b2127d
ref <56499429+referefref@users.noreply.github.com>
2024-07-02 10:48:34 +0800

    Update users.go

--- users.go
+++ users.go
@@ -2,38 +2,6 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"os/exec"
-	"time"
-)
-
-func performScheduledTasks(config *Config) {
-	tasks := selectRandomOrHardcodedScheduledTasks(config.ScheduledTasks.Options, config.ScheduledTasks.SelectionMethod)
-	for _, task := range tasks {
-		cmd := exec.Command("schtasks", "/create", "/tn", task.Name, "/tr", task.Path, "/sc", task.Schedule, "/st", task.StartTime)
-		err := cmd.Run()
-		if err != nil {
-			logToFile(config.General.LogFile, fmt.Sprintf("Failed to create scheduled task %s: %v", task.Name, err))
-		} else {
-			logToFile(config.General.LogFile, fmt.Sprintf("Created scheduled task %s", task.Name))
-		}
-		time.Sleep(time.Duration(config.General.ActionDelay+rand.Intn(2*config.General.RandomnessFactor+1)-config.General.RandomnessFactor) * time.Second)
-	}
-}
-
-func selectRandomOrHardcodedScheduledTasks(options []ScheduledTask, method string) []ScheduledTask {
-	if method == "hardcoded" {
-		return options
-	}
-	rand.Seed(time.Now().UnixNano())
-	return []ScheduledTask{options[rand.Intn(len(options))]}
-}
-
-root@mainwebsite:/home/ref/sinonv2# cat users.go 
-package main
-
-import (
-	"fmt"
 
 	wapi "github.com/iamacarpet/go-win64api"
 )
